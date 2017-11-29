c5b67c69a5b076178d5407ded98373940c84212c
Jelena Nesic <jelenanesicm@gmail.com>
2017-11-29 11:58:40 +0100

    Added ES Lint

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return(
-         <div>
+	render() {
+		return(
+			<div>
 				<p>World Bank Init Project</p>
 				<button type="button" className="btn btn-primary">Primary</button>
-         </div>
-      );
-   }
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 // Bootstrap import 
-import '../node_modules/bootstrap/scss/bootstrap.scss'
+import '../node_modules/bootstrap/scss/bootstrap.scss';
 import './css/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
