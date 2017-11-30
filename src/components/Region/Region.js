3181a9f07e535ca5bc478344d37d9054ffbe0cb0
Jelena Nesic <jelenanesicm@gmail.com>
2017-11-30 16:00:08 +0100

    Show only 8 regions, on click show 8 more

--- src/components/Region/Region.js
+++ src/components/Region/Region.js
@@ -1,15 +1,24 @@
 import React from 'react';
-
+import './regions.scss';
 import regionService from '../../services/regionService';
-
 import SingleRegion from './SingleRegion';
 
 class Region extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			regions: []
+			regions: [],
+			showRegions: 8 //how many regions to show
 		};
+		this.handleShowRegions = this.handleShowRegions.bind(this);
+	}
+
+	handleShowRegions () {
+		this.setState({
+			showRegions:
+				this.state.showRegions >= this.state.regions.length ?
+					this.state.showRegions : this.state.showRegions + 8
+		});
 	}
 
 	componentWillMount() {
@@ -23,15 +32,28 @@ class Region extends React.Component {
 	}
 
 	render() {
-		const { regions } = this.state;
+		const { regions, showRegions } = this.state;
+
+		const slicedRegions = regions.slice(0, showRegions).map(
+			region => <SingleRegion 
+				key={region.id + Math.random()}
+				name={region.name}/>
+		);
 		return(
-			<div className="row col-md-12">
-				{
-					regions.map(region => <SingleRegion 
-						key={region.id + Math.random()}
-						name={region.name}
-					/>)
-				}
+			<div className="regions row col-md-12">
+				<div className="row">
+					{
+						slicedRegions
+					}
+				</div>
+				<div className="col-md-12 aligner">
+					<button 
+						className="btn btn-success" 
+						onClick={this.handleShowRegions}
+					>
+						Show More
+					</button>
+				</div>
 			</div>
 		);
 	}
--- src/components/Region/SingleRegion.js
+++ src/components/Region/SingleRegion.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import './singleRegion.scss';
+import './regions.scss';
 
 const SingleRegion = (props) => {
 	return(
 		<div className="single-region col-md-3">
 			<div className="col-md-8 offset-md-2 wrap-region">
-				<p className="text-center">{props.name}</p>
+				<p className="text-center"><strong>{props.name}</strong></p>
 			</div>
 		</div>
 	);
