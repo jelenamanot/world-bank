0056382ffa5c65a3f86ab065614691b66dfc5057
Jelena Nesic <jelenanesicm@gmail.com>
2017-12-04 14:30:18 +0100

    Finished search countries & show more countries

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -11,9 +11,14 @@ class Country extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			countries: []
+			countries: [],
+			showCountries: 10,
+			searchValue: ''
 		};
+		this.handleShowCountries = this.handleShowCountries.bind(this);
+		this.handleUpdateSearch = this.handleUpdateSearch.bind(this);
 	}
+
 	componentWillMount() {
 		countryService.getAllData()
 			.then(response => {
@@ -24,21 +29,59 @@ class Country extends React.Component {
 			});
 	}
 
+	handleShowCountries() {
+		this.setState({
+			showCountries: this.state.showCountries >= this.state.countries.length ?
+				this.state.showCountries : this.state.showCountries + 10
+		});
+	}
+
+	handleUpdateSearch(e) {
+		this.setState({
+			searchValue: e.target.value
+		});
+	}
+
 	render() {
-		const { countries } = this.state;
+		const { countries, showCountries, searchValue } = this.state;
+		
+		let filteredCountries = countries.filter(country => {
+			return country.name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1;
+		});
+
 		return(
 			<div className="countries col-md-12">
 				<div className="search-wrap row aligner">
-					<Search placeholder="Search countries" />
+					<Search 
+						placeholder="Search countries"
+						searchValue={searchValue}
+						handleUpdateSearch={this.handleUpdateSearch}
+					/>
 				</div>
 				<div className="row">
 					{
-						countries.map(country => <SingleCountry 
-							key={country.id} 
-							name={country.name} 
-							capital={country.capitalCity}
-							region={country.region.value}
-						/>)
+						filteredCountries.length === 0 ?
+							<p>No search results found</p> :
+							filteredCountries.slice(0, showCountries).map(
+								country => <SingleCountry 
+									key={country.id}
+									name={country.name}
+									capital={country.capitalCity}
+									region={country.region.value}
+								/>
+							)
+					}
+				</div>
+				<div className="col-md-12 aligner">
+					{
+						countries.length === showCountries ?
+							null :
+							<button 
+								className="btn btn-success" 
+								onClick={this.handleShowCountries}
+							>
+								Show More
+							</button>
 					}
 				</div>
 			</div>
